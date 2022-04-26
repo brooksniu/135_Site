eaca921c3e914c006deebcff10f0209aced2f52b
Ruijia Brooks Niu <brooksnew92@gmail.com>
2022-04-26 00:15:13 -0700

    fixed python hello page

--- cgi-bin/python-hello-html-world.py
+++ cgi-bin/python-hello-html-world.py
@@ -16,11 +16,11 @@ print ("<body>")
 print ("<h1>Brooks here - Hello, Python!</h1>")
 print ("<p>This page was generated with the Perl programming langauge</p>")
 
-print ("<p>Current Time: " + now.strftime("%d/%m/%Y %H:%M:%S") + "</p>")
+print ("<p>Current Time: ", now.strftime("%d/%m/%Y %H:%M:%S") , "</p>")
 
 # IP Address is an environment variable when using CGI
 address = os.getenv("REMOTE_ADDR")
-print ("<p>Your IP Address: " + address + "</p>")
+print ("<p>Your IP Address: ", address, "</p>")
 
 print ("</body>")
 print ("</html>")
