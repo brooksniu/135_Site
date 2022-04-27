aeecdc3481ae924069893bbec13076bb3417a705
Ruijia Brooks Niu <brooksnew92@gmail.com>
2022-04-26 22:40:40 -0700

    added pyhton post echo

---
+++ cgi-bin/python-post-echo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys
+import json
+
+data = sys.stdin.read()
+
+print ("Cache-Control: no-cache")
+print ("Content-type: text/html\n")
+print ("<html>")
+print ("<head>")
+print ("<title>POST Request Echo</title>")
+print ("</head>")
+print ("<body>")
+print ("<h1 align=center>POST Request Echo</h1>")
+print ("<hr/>")
+
+print ("<b>Message Body: </b><br />")
+print ("<b>",data, "<b><br />")
+
+print ("</body></html>")
