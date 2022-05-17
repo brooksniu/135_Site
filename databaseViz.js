dd3d3cf96db0d84d17d3778d09b0d38b53ef5260
Ruijia Brooks Niu <brooksnew92@gmail.com>
2022-05-17 04:43:22 -0700

    added database html

--- databaseViz.js
+++ databaseViz.js
@@ -1,13 +1,27 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const grid = document.getElementById("grid");
-    let url = 'https://brooksniu.com/api/static/';
+    const staticGrid = document.getElementById("static");
+    const performanceGrid = document.getElementById("performance");
+    const activGrid = document.getElementById("activity");
+    let staticUrl = 'https://brooksniu.com/api/static/';
+    let performanceUrl = 'https://brooksniu.com/api/performance/';
+    let activUrl = 'https://brooksniu.com/api/activity/';
     let username = 'grader';
     let password = 'grader';
     let credentials = btoa(`${username}:${password}`);
     let auth = { "Authorization" : `Basic ${credentials}` };
-    let data = fetch(url, { headers : auth })
+    fetch(staticUrl, { headers : auth })
     .then(response => response.json())
-    .then(json => console.log(json));
-    console.log(data);
-    grid.setAttribute("data", data);
+    .then(json => {
+        staticGrid.setAttribute("data", JSON.stringify(json));
+    });
+    fetch(performanceUrl, { headers : auth })
+    .then(response => response.json())
+    .then(json => {
+        performanceGrid.setAttribute("data", JSON.stringify(json));
+    });
+    fetch(activUrl, { headers : auth })
+    .then(response => response.json())
+    .then(json => {
+        activGrid.setAttribute("data", JSON.stringify(json));
+    });
 })
