22584c8e6d06867db80c31dc130e08d4cada401e
Ruijia Brooks Niu <brooksnew92@gmail.com>
2022-05-18 00:15:33 -0700

    added charts with dummy data

--- databaseViz.js
+++ databaseViz.js
@@ -1,7 +1,9 @@
 window.addEventListener("DOMContentLoaded", () => {
+    const noscriptGrid = document.getElementById("noscript");
     const staticGrid = document.getElementById("static");
     const performanceGrid = document.getElementById("performance");
     const activGrid = document.getElementById("activity");
+    let noscriptUrl = 'https://brooksniu.com/api/getnoscript/';
     let staticUrl = 'https://brooksniu.com/api/static/';
     let performanceUrl = 'https://brooksniu.com/api/performance/';
     let activUrl = 'https://brooksniu.com/api/activity/';
@@ -9,6 +11,11 @@ window.addEventListener("DOMContentLoaded", () => {
     let password = 'grader';
     let credentials = btoa(`${username}:${password}`);
     let auth = { "Authorization" : `Basic ${credentials}` };
+    fetch(noscriptUrl, { headers : auth })
+    .then(response => response.json())
+    .then(json => {
+        noscriptGrid.setAttribute("data", JSON.stringify(json));
+    });
     fetch(staticUrl, { headers : auth })
     .then(response => response.json())
     .then(json => {
